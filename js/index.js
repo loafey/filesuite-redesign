f0d49711c2268a208d29427bf9a6c3d2ca3355e3
samhamnam <samuel.hammersberg@gmail.com>
2018-01-05 01:35:57 +0100

    SStoff

--- js/index.js
+++ js/index.js
@@ -27,3 +27,18 @@ function GreenDis(e) {
         document.getElementById("NewKeyButton").disabled = false;
     }, 3000);
 }
+
+var keysizeSlider = document.getElementById("keySize");
+keysizeSlider.value = 2;
+
+var keysize = keysizeSlider.value * 1024;
+
+keysizeSlider.addEventListener("input", function() {
+    document.getElementById("keySizeText").innerHTML = Math.round(keysizeSlider.value) * 1024;
+    keysize = Math.round(keysizeSlider.value) * 1024;
+    console.log("Keysize: " + keysize);
+})
+
+keysizeSlider.addEventListener("change", function() {
+    keysizeSlider.value = Math.round(keysizeSlider.value);
+})
